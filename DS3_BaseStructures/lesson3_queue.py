b235f5538f9395da6dee55b0931e5da2ec038a9b
wgf0210 <1181478347@qq.com>
2020-09-16 17:01:51 +0800

    队列

--- DS3_BaseStructures/lesson3_queue.py
+++ DS3_BaseStructures/lesson3_queue.py
@@ -113,8 +113,8 @@ class Task:
         return currenttime - self.timestamp
 
 def newPrintTask():
-    num = random.randrange(1,181)
-    if num == 180:
+    num = random.randrange(1,61)
+    if num == 60:
         return True
     else:
         return False
