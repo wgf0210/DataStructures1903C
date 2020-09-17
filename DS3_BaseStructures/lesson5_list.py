41164feff4ef3c9c92bfeb5653166c2e362a35c9
wgf0210 <1181478347@qq.com>
2020-09-17 18:34:59 +0800

    链表实现有序列表和无序列表

--- DS3_BaseStructures/lesson5_list.py
+++ DS3_BaseStructures/lesson5_list.py
@@ -158,6 +158,24 @@ class OrderedList:
                 else:
                     current = current.get_next()
         return found 
+    def add(self,item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.get_data()>item:
+                stop = True
+            else:
+                previous = current
+                current = current.get_next()
+        temp = Node(item)
+        if previous == None:
+            temp.set_next(self.head)
+            self.head = temp
+        else:
+            temp.set_next(current)
+            previous.set_next(temp)
+                
 
     
 
